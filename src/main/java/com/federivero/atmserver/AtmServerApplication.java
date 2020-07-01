027df46736aaaf642f27902c6959d035fbd2905e
FedericoRivero <fedeerivero@gmail.com>
2020-06-30 22:27:19 -0300

    Added User model into db

--- src/main/java/com/federivero/atmserver/AtmServerApplication.java
+++ src/main/java/com/federivero/atmserver/AtmServerApplication.java
@@ -1,7 +1,15 @@
 package com.federivero.atmserver;
 
+import com.federivero.atmserver.entity.UserEntity;
+import com.federivero.atmserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class AtmServerApplication {
@@ -10,4 +18,14 @@ public class AtmServerApplication {
 		SpringApplication.run(AtmServerApplication.class, args);
 	}
 
+	@Autowired
+	private UserRepository repository;
+
+	@PostConstruct
+	public void initUsers() {
+		List<UserEntity> users = Stream.of(
+				new UserEntity("admin", "admin")
+		).collect(Collectors.toList());
+		repository.saveAll(users);
+	}
 }
---
+++ src/main/java/com/federivero/atmserver/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package com.federivero.atmserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USER")
+public class UserEntity {
+    @Id
+    private String userName;
+    private String password;
+}
