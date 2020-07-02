d5ff2dc8547fd9c57b6f59a44a173a306d9a864c
FedericoRivero <fedeerivero@gmail.com>
2020-07-02 02:53:06 -0300

    Refactored ATM search for 2 parameters

--- src/main/java/com/federivero/atmserver/controller/AtmController.java
+++ src/main/java/com/federivero/atmserver/controller/AtmController.java
@@ -3,12 +3,14 @@ package com.federivero.atmserver.controller;
 import com.federivero.atmserver.entity.Atm;
 import com.federivero.atmserver.service.AtmDetailsService;
 import org.json.JSONException;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -19,8 +21,15 @@ public class AtmController {
     private AtmDetailsService atmDetailsService;
 
     @GetMapping("/atm")
-    public List<Atm> atm(@RequestParam(value = "", required = false, defaultValue = "") String text, @RequestParam(value = "s", required = false, defaultValue = "") List<String> fields) throws IOException, JSONException {
-        List<Atm> atmList = atmDetailsService.getAtmList();
+    public List<Atm> atm(@RequestParam(value = "q", required = false, defaultValue = "") String text, @RequestParam(value = "fields", required = false, defaultValue = "") List<String> fields) throws IOException, JSONException {
+
+        List<Atm> atmList = new ArrayList<>();
+
+        if (text==null || fields.isEmpty()){
+            atmList = atmDetailsService.getAtmList();
+        } else {
+            atmList = atmDetailsService.filterAtmList(text, fields);
+        }
         return atmList;
     }
 }
--- src/main/java/com/federivero/atmserver/service/AtmDetailsService.java
+++ src/main/java/com/federivero/atmserver/service/AtmDetailsService.java
@@ -8,6 +8,7 @@ import java.io.Reader;
 import java.lang.reflect.Type;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.federivero.atmserver.entity.Atm;
@@ -47,4 +48,60 @@ public class AtmDetailsService {
         return this.AtmList;
     }
 
+    public List<Atm> filterAtmList(String text, List<String> fields){
+        List<Atm> AtmList = new ArrayList<>();
+
+        for(Atm atm : this.AtmList) {
+            if (fields.contains("street")){
+                if(atm.address.street!=null && atm.address.street.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("housenumber")){
+                if(atm.address.housenumber!=null && atm.address.housenumber.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("postalcode")){
+                if(atm.address.postalcode!=null && atm.address.postalcode.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("city")){
+                if(atm.address.city!=null && atm.address.city.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("lat")){
+                if(atm.address.geoLocation.lat!=null && atm.address.geoLocation.lat.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("lng")){
+                if(atm.address.geoLocation.lng!=null && atm.address.geoLocation.lng.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("distance")){
+                if(atm.distance!=null && atm.distance.toString().equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+            if (fields.contains("type")){
+                if(atm.type!=null && atm.type.equals(text)){
+                    AtmList.add(atm);
+                    continue;
+                }
+            }
+        }
+
+        return AtmList;
+    }
 }
